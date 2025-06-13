57c2501ff33d96310de59895cf755038acb6feb5
CanGuvercin <mustafacanguvercin@gmail.com>
2025-06-13 20:34:08 +0300

    general changes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import React from 'react';
+
 import App from './App.jsx';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
